7926e27ac1f2cbc26fa581efcb835bb7b6b62305
Sy Truong <syytruong@gmail.com>
2021-06-03 13:02:43 +0300

    reverse last commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["i.postimg.cc", "fakestoreapi.com"],
   },
   env: {
-    stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 }
